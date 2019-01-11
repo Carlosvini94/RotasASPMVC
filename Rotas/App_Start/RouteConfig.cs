666ecbb4714a5ecaf5e1d38502ed1c1829abfe07
Carlos Costa <carlos.costa@newtonpaiva.br>
2019-01-11 10:08:22 -0200

    Adicionar arquivos de projeto.

---
+++ Rotas/App_Start/RouteConfig.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Rotas
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Todas as Noticias",
+                url: "TodasNoticias/",
+                defaults: new { controller = "Home", action = "TodasAsNoticias" }
+            );
+
+            routes.MapRoute(
+                name: "Noticias por Categoria",
+                url: "Noticias/{categoria}/",
+                defaults: new { controller = "Home", action = "MostraCategoria" }
+            );
+
+            routes.MapRoute(
+                name: "Noticias",
+                url: "Noticia/{categoria}/{titulo}",
+                defaults: new { controller = "Home", action = "MostraNoticia" }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
